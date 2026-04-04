68bfaae8ac853082f61126e0fcd42a332e8438de
Matheus <matheus@MacBook-Air-de-Matheus.local>
2026-04-04 14:23:27 -0300

    chore: init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import globals from "globals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   {
+    files: ["**/*.spec.{ts,tsx}", "**/tests/**/*.{ts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     rules: {
-      "no-multiple-empty-lines": [
-        "error",
-        { max: 1, maxEOF: 1, maxBOF: 0 },
-      ],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1, maxBOF: 0 }],
     },
   },
   // Override default ignores of eslint-config-next.
@@ -21,6 +27,7 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "src/generated/**",
   ]),
 ]);
 
